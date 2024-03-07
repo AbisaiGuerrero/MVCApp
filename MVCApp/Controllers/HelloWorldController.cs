0ae48ff3c27a690d047c62ea0abae2ef987678b2
nahaliel abisai guerrero guerra <nahaliel.lexmark@gmail.com>
2024-03-06 20:10:13 -0600

    Segundo proyecto

---
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numeTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numeTimes;
+            return View();
+        }
+
+        //Get: /HelloWorld/
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        //Get: /HelloWorld/Welcome/
+        // Requieres using System.Text.Encodings.Web
+        //Get: /HelloWorld/Welcome?name=abisai&numtimes=5
+        //public string Welcome(string name, int numTimes=1)
+        //{
+        //    // return "This is the welcome action method...";
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, Numtimes is: {numTimes}");
+        //}
+    }
+}
